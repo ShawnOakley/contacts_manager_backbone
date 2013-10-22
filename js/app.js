0e8369f8aee4c968df685dc868ea80f883b36969
Shawn <shawnoakley@gmail.com>
2013-10-22 15:21:46 -0400

    Adds basic filter.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-// Basic store of contacts for testing app
+(function ($) {
 
-(function($){
-  var contacts = [
+    //demo data
+    var contacts = [
         { name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
         { name: "Contact 2", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
         { name: "Contact 3", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
@@ -12,58 +12,124 @@
         { name: "Contact 8", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" }
     ];
 
+    //define product model
+    var Contact = Backbone.Model.extend({
+        defaults: {
+            photo: "/img/placeholder.png"
+        }
+    });
 
+    //define directory collection
+    var Directory = Backbone.Collection.extend({
+        model: Contact
+    });
 
-// Contact model
+    //define individual contact view
+    var ContactView = Backbone.View.extend({
+        tagName: "article",
+        className: "contact-container",
+        template: _.template($("#contactTemplate").html()),
 
-  var Contact = Backbone.Model.extend({
-    defaults: {
-      photo: "/Users/appacademy/Desktop/contacts_manager/img/placeholder.png"
-    }
-  });
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
 
-  // Collection of contacts
+    //define master view
+    var DirectoryView = Backbone.View.extend({
+        el: $("#contacts"),
 
-  var Directory = Backbone.Collection.extend({
-    model: Contact
-  });
+        initialize: function () {
+            this.collection = new Directory(contacts);
 
-  var ContactView = Backbone.View.extend({
-    tagName: "article",
-    className: "contact-container",
-    template: $("#contactTemplate").html(),
+            this.render();
+            this.$el.find("#filter").append(this.createSelect());
 
-    render: function () {
-      var tmpl = _.template(this.template);
+            this.on("change:filterType", this.filterByType, this);
+            this.collection.on("reset", this.render, this);
+        },
 
-      this.$el.html(tmpl (this.model.toJSON()));
-      return this;
-    }
-  });
+        render: function () {
+            this.$el.find("article").remove();
 
-  var DirectoryView = Backbone.View.extend({
-    el: $("#contacts"),
+            _.each(this.collection.models, function (item) {
+                this.renderContact(item);
+            }, this);
+        },
 
-    initialize: function () {
-      this.collection = new Directory(contacts);
-      this.render();
-    },
+        renderContact: function (item) {
+            var contactView = new ContactView({
+                model: item
+            });
+            this.$el.append(contactView.render().el);
+        },
 
-    render: function () {
-      var that = this;
-      _.each(this.collection.models, function(item) {
-        that.renderContact(item);
-      }, this);
-    },
+        getTypes: function () {
+            return _.uniq(this.collection.pluck("type"));
+        },
 
-    renderContact: function (item) {
-      var contactView = new ContactView({
-        model:item
-      });
-      this.$el.append(contactView.render().el);
-    }
-  });
+        createSelect: function () {
+            var select = $("<select/>", {
+                    html: "<option value='all'>All</option>"
+                });
 
-  var directory = new DirectoryView();
+            _.each(this.getTypes(), function (item) {
+                var option = $("<option/>", {
+                    value: item,
+                    text: item
+                }).appendTo(select);
+            });
+
+            return select;
+        },
+
+        //add ui events
+        events: {
+            "change #filter select": "setFilter"
+        },
+
+        //Set filter property and fire change event
+        setFilter: function (e) {
+            this.filterType = e.currentTarget.value;
+            this.trigger("change:filterType");
+        },
+
+        //filter the view
+        filterByType: function () {
+            if (this.filterType === "all") {
+                this.collection.reset(contacts);
+                contactsRouter.navigate("filter/all");
+            } else {
+                this.collection.reset(contacts, { silent: true });
+
+                var filterType = this.filterType,
+                    filtered = _.filter(this.collection.models, function (item) {
+                        return item.get("type") === filterType;
+                    });
+
+                this.collection.reset(filtered);
+
+                contactsRouter.navigate("filter/" + filterType);
+            }
+        }
+    });
+
+    var ContactsRouter = Backbone.Router.extend({
+        routes: {
+            "filter/:type": "urlFilter"
+        },
+
+        urlFilter: function (type) {
+            directory.filterType = type;
+            directory.trigger("change:filterType");
+        }
+    });
+
+    var directory = new DirectoryView();
+
+    var contactsRouter = new ContactsRouter();
+
+    Backbone.history.start();
 
 } (jQuery));
